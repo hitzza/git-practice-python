694a997e0a250efbaf61c923875b7956bce011f3
hitzza_home <cslzh4433@gmail.com>
2022-04-27 21:48:20 +0900

    check new computer numpy module

---
+++ ex/vector_ex.py
@@ -0,0 +1,7 @@
+#vector란? 숫자를 원소로 가지는 list 또는 array
+#벡터의 norm 노름은 원점에서부터의 거리
+#l1 노름은 각 성분의 변화량의 절대값을 모두 더한값
+#l2 노름은 피타고라스 정리를 이용해 유클리드 거리를 계산
+#import numpy as np
+#def l1_norm(x):
+#    x_norm = np.abs(x)
