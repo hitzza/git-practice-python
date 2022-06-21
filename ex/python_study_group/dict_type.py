ab1187b62288c01d84f0dcb4a47e5a5150154fbe
DESKTOP-7SUMSRN\cslzh <cslzh4433@gmail.com>
2022-06-21 20:41:16 +0900

    how to print use dict index

---
+++ ex/python_study_group/dict_type.py
@@ -0,0 +1,13 @@
+#how to print use dict_type index
+
+a = {'car' : ['bus', 'truck', 'taxi'], 'train' : 'ktx'}
+a_key = a.keys()
+a_values = a.values()
+print(a_key)
+print(a_values)
+
+list_key = list(a_key)
+list_values = list(a_values)
+print(list_key)
+print(list_values)
+
--- ex/python_study_group/test.py
+++ ex/python_study_group/test.py
@@ -24,3 +24,4 @@ kwpacking(a=1,b=2,c=3)#dict type
 a = [1,2,3,4,4,5,1,2]
 
 print(set(a))
+print(1e-2)
