8911bb8da4b3192412912276cfb94d3f0086ad6a
DESKTOP-7SUMSRN\cslzh <cslzh4433@gmail.com>
2022-06-23 14:53:18 +0900

    baekjoon 2588 solve value error

--- ex/python_study_group/bj_2588.py
+++ ex/python_study_group/bj_2588.py
@@ -9,7 +9,23 @@ print(b_sec)
 print(b_third)
 print(b_first + b_sec*10 + b_third*100)
 '''
+'''
 a,b = map(int,input().split())
+b_first = b%10
+b_sec = b //10 %10
+b_third = b//100
+b_first *= a
+b_sec *= a
+b_third *= a
+
+print(b_first)
+print(b_sec)
+print(b_third)
+print(b_first + b_sec*10 + b_third*100)
+'''
+a = int(input())
+b = int(input())
+
 b_first = b%10
 b_sec = b //10 %10
 b_third = b//100
