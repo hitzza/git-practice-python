8ae7dfdb45c51ad9ab18fd8782eb051e92c2a15e
DESKTOP-7SUMSRN\cslzh <cslzh4433@gmail.com>
2022-06-23 19:15:14 +0900

    bj_2753, 14681. 2884, 2525

--- ex/python_study_group/bj_1330_2480.py
+++ ex/python_study_group/bj_1330_2480.py
@@ -26,3 +26,68 @@ else:
     print("F")
 '''
 #2753
+'''
+check_year = int(input())
+check_1 = check_year // 100
+check_2 = check_year % 100
+if check_year % 4 ==0:
+    if check_1 % 4 != 0:
+        if check_2 == 0:
+            print('0')
+        else:
+            print('1')
+    else:
+        print('1')
+else:
+    print('0') 
+'''
+#14681
+'''
+a = int(input())
+b = int(input())
+
+if a > 0 and b > 0:
+    print(1)
+elif a < 0 and b > 0:
+    print(2)
+elif a < 0 and b < 0:
+    print(3)
+elif a > 0 and b < 0:
+    print(4)
+'''
+#2884
+'''
+h, m = map(int,input().split())
+
+if 0 <= h <= 23 and 0 <= m <= 59:
+    if m - 45 < 0:
+        if h == 0:
+            h += 23
+            m += 15
+            print(h, m)
+        else: 
+            h -= 1
+            m += 15
+            print(h, m)
+    else:
+        m -= 45
+        print(h, m)
+'''
+#2525
+'''
+h,m = map(int,input().split())
+c = int(input())
+m_timer = c % 60
+h_timer = c // 60
+
+m += m_timer
+h += h_timer
+if m >= 60:
+    m -= 60
+    h += 1
+    h %= 24
+    print(h,m)
+else:
+    h %= 24
+    print(h,m)
+'''
