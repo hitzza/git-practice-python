5ae87547e24437102fdca021ab179f4d360565da
DESKTOP-7SUMSRN\cslzh <cslzh4433@gmail.com>
2022-06-23 15:10:11 +0900

    bj1330- 9498

---
+++ ex/python_study_group/bj_1330_2480.py
@@ -0,0 +1,28 @@
+#1330
+'''
+a,b = map(int,input().split())
+
+if a >= -10000 and a <=10000 and b >= -10000 and b <=10000:
+    if a>b:
+        print(">")
+    elif a<b:
+        print("<")
+    elif a==b:
+        print("==")
+'''
+#9498
+'''
+score = int(input())
+
+if 90 <= score <= 100:
+    print("A")
+elif 80 <= score < 90:
+    print("B")
+elif 70 <= score < 80:
+    print("C")
+elif 60 <= score < 70:
+    print("D")
+else:
+    print("F")
+'''
+#2753
